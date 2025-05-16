076a59558978d32b72ff063b3d5db23a97cd23f0
Mário <mariobalen3@gmail.com>
2025-05-16 01:07:57 -0300

    atualizacoes feitas em aula

--- greating-service/src/main/java/br/edu/atitus/greating_service/GreatingController.java
+++ greating-service/src/main/java/br/edu/atitus/greating_service/GreatingController.java
@@ -1,7 +1,5 @@
 package br.edu.atitus.greating_service;
 
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,28 +12,25 @@ import br.edu.atitus.greating_service.configs.GreatingConfig;
 @RestController
 @RequestMapping("/greating-service")
 public class GreatingController {
-	
-//	@Value("${greating-service.greating}")
-//	private String greating;
-//	
-//	@Value("${greating-service.default-name}")
-//	private String defaultName;
-	
-	private final GreatingConfig greatingConfig;
-	
-	public GreatingController(GreatingConfig greatingConfig) {
-	super();
-	this.greatingConfig = greatingConfig;
-}
 
-	@GetMapping({"", "/", "/{namePath}"})
-	public ResponseEntity<String> getGreating(
-			@PathVariable(required = false) String namePath,
-			@RequestParam(required = false) String name
-			) {
+
+	// @Autowired
+	private final GreatingConfig greatingConfig;
+
+	public GreatingController(GreatingConfig greatingConfig) {
+		super();
+		this.greatingConfig = greatingConfig;
+	}
+
+	@GetMapping({ "", "/", "/{namePath}" })
+	public ResponseEntity<String> getGreating(@PathVariable(required = false) String namePath,
+			@RequestParam(required = false) String name) {
 		if (name == null)
 			name = namePath != null ? namePath : greatingConfig.getDefaultName();
+
 		String returnText = String.format("%s %s!", greatingConfig.getGreating(), name);
+
 		return ResponseEntity.ok(returnText);
 	}
+
 }
