7427d691de9eaca0ff70c92cacef25df981371f1
Mário <mariobalen3@gmail.com>
2025-05-30 19:09:48 -0300

    Implementando Resilience e Cache no Product-Service

--- product-service/src/main/java/br/edu/atitus/product_service/controller/OpenProductController.java
+++ product-service/src/main/java/br/edu/atitus/product_service/controller/OpenProductController.java
@@ -1,6 +1,7 @@
 package br.edu.atitus.product_service.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,35 +18,52 @@ import br.edu.atitus.product_service.repositories.ProductRepository;
 public class OpenProductController {
 	private final ProductRepository repo;
 	private final CurrencyClient currencyClient;
+	private final CacheManager cacheManager;
 
-	public OpenProductController(ProductRepository repo, CurrencyClient currencyClient) {
+	public OpenProductController(ProductRepository repo, CurrencyClient currencyClient, CacheManager cacheManager) {
 		super();
 		this.repo = repo;
 		this.currencyClient = currencyClient;
+		this.cacheManager = cacheManager;
 	}
-	
+
 	@Value("${server.port}")
 	private int serverPort;
-	
+
 	@GetMapping("/{idProduct}/{targetCurrency}")
-	public ResponseEntity<ProductEntity> getProduct(
-				@PathVariable Long idProduct,
-				@PathVariable String targetCurrency
-			) throws Exception{
-		ProductEntity product = repo.findById(idProduct)
-				.orElseThrow(()-> new Exception("Product Unsupported"));
-		
-		product.setConvertedPrice(product.getPrice());
-		product.setEnviroment("Product-service running on port: " + serverPort);
-		
-		if(targetCurrency.equals(product.getCurrency()))
+	public ResponseEntity<ProductEntity> getProduct(@PathVariable Long idProduct, @PathVariable String targetCurrency)
+			throws Exception {
+
+		targetCurrency = targetCurrency.toUpperCase();
+		String nameCache = "Product";
+		String keyCache = idProduct + targetCurrency;
+
+		ProductEntity product = cacheManager.getCache(nameCache).get(keyCache, ProductEntity.class);
+
+		if (product == null) {
+			product = repo.findById(idProduct).orElseThrow(() -> new Exception("Product Unsupported"));
+
 			product.setConvertedPrice(product.getPrice());
-		else {
-			CurrencyResponse currency = currencyClient.getCurrency(product.getPrice(), product.getCurrency(), targetCurrency);
-			product.setConvertedPrice(currency.getConvertedValue());
-			product.setEnviroment(product.getEnviroment() + " - " + currency.getEnviroment());
+			product.setEnviroment("Product-service running on port: " + serverPort);
+
+			if (targetCurrency.equals(product.getCurrency()))
+				product.setConvertedPrice(product.getPrice());
+			else {
+				CurrencyResponse currency = currencyClient.getCurrency(product.getPrice(), product.getCurrency(),
+						targetCurrency);
+				if (currency != null) {
+					product.setConvertedPrice(currency.getConvertedValue());
+					product.setEnviroment(product.getEnviroment() + " - " + currency.getEnviroment());
+					
+					cacheManager.getCache(nameCache).put(keyCache, product);
+				} else {
+					product.setConvertedPrice(-1);
+					product.setEnviroment(product.getEnviroment() + " - Currency unavalaible");
+				}
+			}
+		} else {
+			product.setEnviroment("Product-service running on port: " + serverPort + " - DataSource: cache" );
 		}
-		
 		return ResponseEntity.ok(product);
 	}
 }
